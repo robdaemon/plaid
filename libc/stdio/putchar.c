6e20ba9ae015bd8185aee56ab32f34c67d433b3e
Robert Roland <robert@robertroland.org>
2015-08-23 01:11:34 -0700

    The "meaty skeleton" from wiki.osdev.org
    Slightly renamed from myos to plaid.

---
+++ libc/stdio/putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#if defined(__is_plaid_kernel)
+#include <kernel/tty.h>
+#endif
+
+int putchar(int ic) {
+#if defined(__is_plaid_kernel)
+  char c = (char)ic;
+  terminal_write(&c, sizeof(c));
+#else
+  // TODO: Implement this syscall
+#endif
+  
+  return ic;
+}
